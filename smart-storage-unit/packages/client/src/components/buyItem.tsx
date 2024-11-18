63b207fa19f02d062d5534edc530fd8551c9ed7c
CCP Red Dragon <181853434+CCP-Red-Dragon@users.noreply.github.com>
2024-11-18 11:11:48 +0000

    SSU Client

---
+++ smart-storage-unit/packages/client/src/components/buyItem.tsx
@@ -0,0 +1,70 @@
+import { useMUD } from "../MUDContext";
+import React, { useRef, useState } from "react";
+import { EveButton, EveInput } from "@eveworld/ui-components";
+import { formatEther, parseEther } from "viem";
+
+const BuyItem = React.memo(function BuyItem({
+	smartAssemblyId,
+	itemOutId,
+}: {
+	smartAssemblyId: bigint;
+	itemOutId: string;
+}) {
+	const [itemPriceWei, setItemPriceWei] = useState<number | undefined>();
+	const [itemQuantity, setItemQuantity] = useState<number | undefined>();
+	const [erc20Balance, setErc20Balance] = useState<bigint | undefined>();
+
+	const {
+		network: { walletClient },
+		systemCalls: {
+			setItemPrice,
+			getItemPriceData,
+			purchaseItem,
+			getErc20Balance,
+		},
+	} = useMUD();
+
+	const fetchItemPriceData = async () => {
+		const itemPriceData = await getItemPriceData();
+		setItemPriceWei(Number(itemPriceData?.price ?? 0));
+	};
+
+	const itemPriceWeiValueRef = useRef(0);
+
+	const handleEdit = (
+		refString: React.MutableRefObject<number>,
+		eventString: number
+	): void => {
+		refString.current = eventString;
+	};
+
+	return (
+		<>
+
+			<div className="Quantum-Container my-4">
+				<div>Trade Item</div>
+				<div className="flex items-start flex-col gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={itemQuantity}
+						fieldName={"item quantity"}
+						onChange={(str) => setItemQuantity(Number(str))}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await purchaseItem(itemQuantity);
+							}}
+						>
+							Purchase items
+						</EveButton>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+});
+
+export default BuyItem;
---
+++ smart-storage-unit/packages/client/src/components/setRatio.tsx
@@ -0,0 +1,108 @@
+import { useMUD } from "../MUDContext";
+import React, { useRef, useState } from "react";
+import { EveButton, EveInput } from "@eveworld/ui-components";
+import { formatEther, parseEther } from "viem";
+
+const SetRatio = React.memo(function SetRatio({
+	smartAssemblyId,
+	itemInId,
+	itemOutId
+}: {
+	smartAssemblyId: bigint;
+	itemInId: string;
+	itemOutId: string;
+}) {
+	const [itemStackMultiple, setItemStackMultiple] = useState<
+		number | undefined
+	>();
+	const [itemPriceWei, setItemPriceWei] = useState<number | undefined>();
+	const [sellQuantity, setSellQuantity] = useState<number | undefined>();
+
+	const {
+		systemCalls: { setSellConfig, SetRatio, getItemSellData },
+	} = useMUD();
+
+	const fetchItemSellData = async () => {
+		const sellPriceData = await getItemSellData();
+		setItemStackMultiple(Number(sellPriceData?.enforcedItemMultiple ?? 0));
+		setItemPriceWei(Number(sellPriceData?.tokenAmount ?? 0));
+	};
+
+	const itemMultipleValueRef = useRef(0);
+	const itemPriceWeiValueRef = useRef(0);
+
+	const handleEdit = (
+		refString: React.MutableRefObject<number>,
+		eventString: number
+	): void => {
+		refString.current = eventString;
+	};
+
+	return (
+		<>
+			<div className="Quantum-Container my-4">
+				<div>ADMIN - Set Ratios inventory item ID: {itemInId}</div>
+				<div className="text-xs">
+					You can change this inventory item ID in the .env file
+				</div>
+				<div className="flex items-center">
+					<EveButton
+						className="mr-2"
+						typeClass="tertiary"
+						onClick={async (event) => {
+							event.preventDefault();
+							fetchItemSellData();
+						}}
+					>
+						Fetch
+					</EveButton>
+					<div className="flex flex-col">
+						<span className="text-xs">
+							{itemPriceWei && itemStackMultiple
+								? `Every ${itemStackMultiple} units of item ${itemInId} can be sold for ${formatEther(BigInt(itemPriceWei))} EVE`
+								: "No sell config set"}
+						</span>
+					</div>
+				</div>
+
+				<div className="mt-4">Set item ratios</div>
+				<div className="text-xs">
+				For this step, you must be connected as the <b>smart assembly owner</b>.
+				</div>
+				<div className="flex flex-col items-start gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={1}
+						fieldName={`Item in: ${itemInId}`}
+						onChange={(str) => handleEdit(itemMultipleValueRef, str as number)}
+					></EveInput>
+
+					<EveInput
+						inputType="numerical"
+						defaultValue={undefined}
+						fieldName={`Item out: ${itemOutId}`}
+						onChange={(str) => handleEdit(itemPriceWeiValueRef, str as number)}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await setSellConfig(
+									smartAssemblyId,
+									itemMultipleValueRef.current,
+									parseEther(itemPriceWeiValueRef.current.toString())
+								);
+								fetchItemSellData();
+							}}
+						>
+							Set Item Price
+						</EveButton>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+});
+
+export default SetRatio;
